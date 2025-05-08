de863ac562d883a9ebb0e4d4ebdab32d192f00d6
Lucas7rogero <lucas.rogero@sou.inteli.edu.br>
2025-05-08 16:26:02 -0300

    fix: arruma o index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Celso baitola");
+  res.send("Projeto do Afonso");
 });
 
 module.exports = router;
