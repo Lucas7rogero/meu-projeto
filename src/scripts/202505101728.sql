3df93b2e5f6acf253f917aa8236e8acdbdec536a
Lucas7rogero <lucas.rogero@sou.inteli.edu.br>
2025-05-12 20:33:21 -0300

    fix: arruma alguns dos erros

--- src/scripts/202505101728.sql
+++ src/scripts/202505101728.sql
@@ -1,11 +1,9 @@
--- Criação da tabela de usuários
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255),
     email VARCHAR(255)
 );
 
--- Criação da tabela de eventos
 CREATE TABLE events (
     id INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(255),
@@ -14,7 +12,6 @@ CREATE TABLE events (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- Criação da tabela de inscrições
 CREATE TABLE subscriptions (
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
